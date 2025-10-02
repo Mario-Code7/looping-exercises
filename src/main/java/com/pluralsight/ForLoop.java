f3d05742bcffc809a1d261f6427d80ce00a3bbf3
Mario-Code7 <marioroa71@yahoo.com>
2025-10-02 15:47:21 -0500

    exercise completed

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 11; i++) {
+        for (int i = 10; i > 0; i--) {
             Thread.sleep(1000);
             System.out.println(i);
         }
