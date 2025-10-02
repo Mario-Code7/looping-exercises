280f251867f00224fbee5bc1f8c42ba1893618af
Mario-Code7 <marioroa71@yahoo.com>
2025-10-02 15:20:45 -0500

    exercise completed

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            i = i + 1;
+            System.out.println("I love Java");
+        } while (i <= 4);
+    }
+}
